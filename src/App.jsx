07691f03fd37a6ce496ac6e6288904f748be375d
batufidan <batuhan.fidan@ogr.iu.edu.tr>
2024-09-27 22:40:30 +0300

    [CREATE] project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <h1 className="text-underline text-2xl text-white">Batuhan Fidan</h1>
+    </main>
+  );
+};
+
+export default App;
